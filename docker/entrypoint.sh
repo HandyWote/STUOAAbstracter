d4b201f2fde248414046903811cda2e2805f6122
HandyWote <18666119673@163.com>
2025-09-26 07:19:24 +0800

    build(docker): 优化入口脚本的日志输出与计划任务展示方式

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,12 +5,11 @@ cron_file=/etc/cron.d/oap
 
 cp /app/docker/cronjob "$cron_file"
 chmod 0644 "$cron_file"
-crontab "$cron_file"
 
 touch /var/log/oap.log
 touch /var/log/cron.log
 
 echo "Cron service启动，计划任务:"
-crontab -l
+cat "$cron_file"
 
 exec cron -f -L 15
